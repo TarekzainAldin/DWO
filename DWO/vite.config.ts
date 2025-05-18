b7a2ca4467e1be485369ac12b262b633a80bb8da
TAREKZAINALDIN <tarekzindin@gmail.com>
2025-05-18 12:38:11 +0200

    deploay the site on the github

--- DWO/vite.config.ts
+++ DWO/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/DWO/",       // <--- أضف هذا السطر
   server: {
     host: "::",
     port: 8080,
